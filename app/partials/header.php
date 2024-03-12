3a4b3a21e52fc667b654194ebc19597255bc8991
ChuaTeSimp <nhatdi2710@gmail.com>
2024-03-12 17:52:12 +0700

    add Font, basing default CSS

--- app/partials/header.php
+++ app/partials/header.php
@@ -3,10 +3,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <title>Devtalk</title>
-    <link rel="icon" href="/../src/imgs/icons/devtalk-logo.png" type="image/x-icon"/>
+
+    <!-- Font Family -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400&display=swap" rel="stylesheet">
 
     <!-- Bootstrap CSS Supporter -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    
     <!-- CSS Customizing -->
     <style>
         <?php require __DIR__ . '/../src/css/style.css'; ?>
